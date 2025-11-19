1f60f61e5c16bd1e92cc49c1424775691cef61f2
Le Trung Duc <letrungduchp1982@gmail.com>
2025-11-19 21:59:05 +0700

    mẫu truy vẫn bằng pymysql connection và cursor

--- payroll/views.py
+++ payroll/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+import pymysql.cursors
+from django.db import connection
+from pymysql.cursors import DictCursor
+from django.conf import settings
 
+
+conn_settings = settings.DATABASES['default']
+conn = pymysql.connect(
+    host=conn_settings['HOST'],
+    user=conn_settings['USER'],
+    password=conn_settings['PASSWORD'],
+    database=conn_settings['NAME'],
+    port=int(conn_settings.get('PORT', 3306)),
+    charset='utf8mb4',
+    cursorclass=DictCursor
+)
 # Create your views here.
+def viewStaffSalary(request):
+    cursor=conn.cursor(DictCursor)
+    sql='''
+        SELECT s1.username, s2.rank, s2.amount, s2.multiplier
+        from staffprofile s
+        join person s1 on s1.id=s.staff_id
+        join salary s2 on s2.salary_id=s.salary_id
+    '''
+    cursor.execute(sql)
+    view_salary=cursor.fetchall()
+    cursor.close()
+
+    return render(request, 'view_staff_salary.html', {'view_salary':view_salary})
--- payroll_project/urls.py
+++ payroll_project/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
     # KẾT NỐI CÁC APP CON 
     #path('users/', include('users.urls', namespace='users')),
-    #path('payroll/', include('payroll.urls', namespace='payroll')),
+    path('payroll/', include('payroll.urls', namespace='payroll')),
     #path('attendance/', include('attendance.urls', namespace='attendance')),
     #path('funds/', include('funds.urls', namespace='funds')),
 ]
