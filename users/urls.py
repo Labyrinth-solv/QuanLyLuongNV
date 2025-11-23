746c6529126e2da4deac97c021c06f29753546bf
Enanann <[trantiendungxg@gmail.com]>
2025-11-23 17:18:13 +0700

    Thêm các đường dẫn cho users

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+app_name='users'
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile_view, name='profile'),
+    path('admin_dashboard/', views.admin_dashboard_view, name='admin_dashboard')
+]
